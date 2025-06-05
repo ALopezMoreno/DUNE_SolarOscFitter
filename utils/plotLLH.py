fixed likelihood bug. Added integration of main systematics to llh scan and normalisation for plotting

utils/plotLLH.py
@@ -51,12 +51,21 @@ lim_th12, lim_th13, matrix1 = read_llh(th12th13_path)
 
 fig, axs = plt.subplots(1, 3, figsize=(20, 5))
 
-
 # First plot
 im1 = axs[0].imshow(matrix1.T, cmap=plotting.parula_map, aspect='auto', origin='lower',
-                     extent=[lim_th12[0], lim_th12[1], lim_th13[0], lim_th13[1]], vmin=np.max([lower, np.min(matrix1)]), vmax=0)
+                     extent=[lim_th12[0], lim_th12[1], lim_th13[0], lim_th13[1]])#, vmin=np.max([lower, np.min(matrix1)]), vmax=0)
+
+X = np.linspace(lim_th12[0], lim_th12[1], matrix1.shape[1])
+Y = np.linspace(lim_th13[0], lim_th13[1], matrix1.shape[0])
+XX, YY = np.meshgrid(X, Y)
+contours = axs[0].contour(XX, YY, matrix1.T, 
+                         levels=[-9, -4, -1],
+                         colors='white', 
+                         linewidths=2, linestyles='-') 
 
 axs[0].plot(0.307, 0.022, 'r+', markersize=10, markeredgewidth=2)
+axs[0].axhline(y=0.022, color='r', linestyle='dashed', linewidth=1) 
+axs[0].axvline(x=0.307, color='r', linestyle='dashed', linewidth=1) 
 axs[0].set_ylabel(r'$\sin^2\theta_{13}$')
 axs[0].set_xlabel(r'$\sin^2\theta_{12}$')
 axs[0].set_box_aspect(1)
@@ -69,12 +78,21 @@ fig.colorbar(im1, ax=axs[0], label=r'$-\log\mathcal{L}$', fraction=0.05, pad=0.1
 lim_th12, lim_dm21, matrix2 = read_llh(th12dm21_path)
 
 
-
 # Second plot
 im2 = axs[1].imshow(matrix2.T, cmap=plotting.parula_map, aspect='auto', origin='lower',
-                     extent=[lim_th12[0], lim_th12[1], lim_dm21[0]*10000, lim_dm21[1]*10000], vmin=np.max([lower, np.min(matrix2)]), vmax=0)
+                     extent=[lim_th12[0], lim_th12[1], lim_dm21[0]*10000, lim_dm21[1]*10000])#, vmin=np.max([lower, np.min(matrix2)]), vmax=0)
+
+X = np.linspace(lim_th12[0], lim_th12[1], matrix2.shape[1])
+Y = np.linspace(lim_dm21[0]*10000, lim_dm21[1]*10000, matrix2.shape[0])
+XX, YY = np.meshgrid(X, Y)
+contours = axs[1].contour(XX, YY, matrix2.T, 
+                         levels=[-9, -4, -1],
+                         colors='white', 
+                         linewidths=2.5, linestyles='-') 
 
 axs[1].plot(0.307, 0.75, 'r+', markersize=10, markeredgewidth=2)
+axs[1].axhline(y=0.75, color='r', linestyle='dashed', linewidth=1) 
+axs[1].axvline(x=0.307, color='r', linestyle='dashed', linewidth=1) 
 axs[1].set_xlabel(r'$\sin^2\theta_{12}$')
 axs[1].set_ylabel(r'$\Delta m^2_{21} (\times 10^{-4}eV^2)$')
 axs[1].xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
@@ -83,17 +101,26 @@ axs[1].xaxis.offsetText.set_visible(False)
 axs[1].set_box_aspect(1)
 
 # Create a colorbar for the second subplot
-fig.colorbar(im1, ax=axs[1], label=r'$-\log\mathcal{L}$', fraction=0.05, pad=0.1)
+fig.colorbar(im2, ax=axs[1], label=r'$-\log\mathcal{L}$', fraction=0.05, pad=0.1)
 
 # Read the likelihood data for sin2_th13 vs dm2_21
 lim_th13, lim_dm21, matrix3 = read_llh(th13dm21_path)
 
 # Third plot
 im3 = axs[2].imshow(matrix3.T, cmap=plotting.parula_map, aspect='auto', origin='lower',
-                    extent=[lim_th13[0], lim_th13[1], lim_dm21[0]*10000, lim_dm21[1]*10000], 
-                    vmin=np.max([lower, np.min(matrix3)]), vmax=0)
+                    extent=[lim_th13[0], lim_th13[1], lim_dm21[0]*10000, lim_dm21[1]*10000])#, vmin=np.max([lower, np.min(matrix3)]), vmax=0)
+
+X = np.linspace(lim_th13[0], lim_th13[1], matrix3.shape[1])
+Y = np.linspace(lim_dm21[0]*10000, lim_dm21[1]*10000, matrix3.shape[0])
+XX, YY = np.meshgrid(X, Y)
+contours = axs[2].contour(XX, YY, matrix3.T,
+                         levels=[-9, -4, -1],
+                         colors='white', 
+                         linewidths=2, linestyles='-') 
 
 axs[2].plot(0.022, 0.75, 'r+', markersize=10, markeredgewidth=2)
+axs[2].axhline(y=0.75, color='r', linestyle='dashed', linewidth=1) 
+axs[2].axvline(x=0.022, color='r', linestyle='dashed', linewidth=1) 
 axs[2].set_xlabel(r'$\sin^2\theta_{13}$')
 axs[2].set_ylabel(r'$\Delta m^2_{21} (\times 10^{-4}eV^2)$')
 axs[2].xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
@@ -102,7 +129,7 @@ axs[2].xaxis.offsetText.set_visible(False)
 axs[2].set_box_aspect(1)
 
 # Create a colorbar for the third subplot
-fig.colorbar(im1, ax=axs[2], label=r'$-\log\mathcal{L}$', fraction=0.05, pad=0.1)
+fig.colorbar(im3, ax=axs[2], label=r'$-\log\mathcal{L}$', fraction=0.05, pad=0.1)
 
 
 