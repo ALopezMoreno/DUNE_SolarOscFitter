365f8573c48fec9cd76bd9a34fcbf4c04bc8b1ef
AndresLopezMoreno <andres.lopez.moreno@gmail.com>
2026-04-15 16:32:05 +0200

    Remove unintended files from tracking; update .gitignore

--- runFits.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for x in 1 2 3 4; do
-    echo "Running config$x.yaml"
-    julia -t 10 src/readConfig.jl "configs/config$x.yaml"
-done
--- run_several.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-  julia -t 10 src/readConfig.jl config.yaml
-done
